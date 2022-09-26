c57a3bc2eba9aaf149b6c61433a2f1ff3227f049
JonathanLCunnane <jonathan.cunnane@gmail.com>
2022-09-26 19:35:20 +0100

    Optimised solution.

--- Problem095.py
+++ Problem095.py
@@ -11,18 +11,13 @@ def proper_divisors(n: int):
     return divs
 
 upto = 1000000
-found = {}
 skip = set()
 longest_chain = set()
 longest_chain_length = 0
 for n in range(1, upto+1):
     if n in skip:
         continue
-    if n in found:
-        divs = found[n]
-    else:
-        divs = proper_divisors(n)
-        found[n] = divs
+    divs = proper_divisors(n)
     chain = [n]
     while True:
         next_addition = sum(divs)
@@ -33,18 +28,11 @@ for n in range(1, upto+1):
             if chain_len > longest_chain_length:
                 longest_chain_length = chain_len
                 longest_chain = chain[chain.index(next_addition):]
-            #chain.append(next_addition)
-            #print(chain, chain_len)
             break
         if next_addition == 1:
-            #chain.append(next_addition)
-            #print(chain)
             break
         chain.append(next_addition)
-        if next_addition in found:
-            divs = found[next_addition]
-        else:
-            divs = proper_divisors(next_addition)
+        divs = proper_divisors(next_addition)
     skip.update(chain)
     
 
